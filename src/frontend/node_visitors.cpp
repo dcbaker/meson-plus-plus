4e433828c3c0a17ab08198dfcd90a08f8ddbec72
Dylan Baker <dylan@pnwbakers.com>
2021-03-25 17:24:36 -0700

    frontend: Add a visitor to replace subdir() with more statements
    The idea here is that we'll have an AST visitor that will walk all of
    the nodes, replacing each FunctionCall that's a subdir() call with the
    StatementList from that subdir.

--- src/frontend/node_visitors.cpp
+++ src/frontend/node_visitors.cpp
@@ -1,9 +1,48 @@
 // SPDX-license-identifier: Apache-2.0
 // Copyright © 2021 Intel Corporation
 
+#include <filesystem>
+
 #include "node_visitors.hpp"
+#include "driver.hpp"
 
 namespace Frontend::AST {
 
+std::optional<std::unique_ptr<CodeBlock>> SubdirVisitor::operator()(const std::unique_ptr<Statement> & stmt) const {
+    const auto & func = *std::get_if<std::unique_ptr<FunctionCall>>(&stmt->expr);
+    if (func == nullptr) { return std::nullopt; }
+
+    // Meson functions are not first class, so we know that if the type is not
+    // an identifier it's not what we want. The other option would be a
+    // `GetAttribute` (a method).
+    const auto & id = *std::get_if<std::unique_ptr<Identifier>>(&func->id);
+    if (id == nullptr) { return std::nullopt; }
+
+    if (id->value != "subdir") { return std::nullopt; }
+
+    auto const & args = func->args->positional;
+
+    // Since we don't have any other kind of validation, and I'm not sure if we
+    // can validate this at the AST level since we don't have strong typing
+    //
+    // This is unrecoverable, so just throwing here should be fine
+    if (args.size() < 1) {
+        // TODO: use the location data.
+        // TODO: have our own exception class, catch it at the base and print a
+        // nice message
+        throw std::exception{};
+    }
+
+    auto const & dir = *std::get_if<std::unique_ptr<String>>(&args[0]);
+    if (dir == nullptr) {
+        throw std::exception{};
+    }
+
+    // This assumes that the filename is foo/meson.build
+    std::filesystem::path p{*id->loc.begin.filename};
+
+    Driver drv{};
+    return drv.parse(p.stem() / dir->value / "meson.build");
+};
 
 } // namespace Frontend::AST
--- src/frontend/node_visitors.hpp
+++ src/frontend/node_visitors.hpp
@@ -3,8 +3,33 @@
 
 #pragma once
 
+#include <optional>
+
 #include "node.hpp"
 
 namespace Frontend::AST {
+
+/**
+ * A visitor that walks over a statement list, and invokes a new parser for
+ * that file, then returns the the new CodeBlock
+ */
+struct SubdirVisitor {
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<Statement> &) const;
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<IfStatement> &) const {
+        return std::nullopt;
+    };
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<ForeachStatement> &) const {
+        return std::nullopt;
+    };
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<Assignment> &) const {
+        return std::nullopt;
+    };
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<Break> &) const {
+        return std::nullopt;
+    };
+    std::optional<std::unique_ptr<CodeBlock>> operator()(const std::unique_ptr<Continue> &) const {
+        return std::nullopt;
+    };
+};
 
 } // namespace Frontend::AST
