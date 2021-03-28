ce4c2eaf1e44a8075f087e458679439613ba3d6b
Dylan Baker <dylan@pnwbakers.com>
2021-03-28 00:37:20 -0700

    hir: add an abstraction over arguments.
    unlike Meson which uses gnu style arguments internally and tries to
    translate those at the last second, Meson++ uses an abstraction, which
    is lowered using a compiler/linker/archiver method, or when lowering
    into the backend IR.

---
+++ src/hir/arguments.hpp
@@ -0,0 +1,39 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Dylan Baker
+
+/* Abstractions for command line arguments.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace HIR::Arguments {
+
+/**
+ * The kind of argument that this is
+ */
+enum class Type {
+    /// A Pre-processor define (ex, -D...)
+    DEFINE,
+    /// A library to link with (Ex, -lfoo or /path/to/foo.a)
+    LINK,
+    /// A path to search for libraries (ex, -L...)
+    LINK_SEARCH,
+    /// An argument we don't know how to classify, proxy it along
+    RAW,
+};
+
+/**
+ * An abstract argument container
+ */
+class Argument {
+  public:
+    Argument(const std::string & v, const Type & t) : value{v}, type{t} {};
+    virtual ~Argument(){};
+
+    const std::string value;
+    const Type type;
+};
+
+} // namespace HIR::Arguments
