c4b3c42ba423490f2ae2e529106c641527269ff2
Dylan Baker <dylan@pnwbakers.com>
2021-04-01 11:58:57 -0700

    toolchains: commit files that got missed...

---
+++ src/hir/toolchains/detect_archivers_test.cpp
@@ -0,0 +1,21 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Intel Corporation
+// Copyright © 2021 Dylan Baker
+
+#include <gtest/gtest.h>
+
+#include "archiver.hpp"
+
+TEST(detect_archivers, gnu) {
+    // TODO: there are multiple binaries called ar, how can we be sure we're
+    // getting the one we expect?
+
+    // Skip if we don't have g++
+    if (system("ar") == 127) {
+        GTEST_SKIP();
+    }
+    const auto comp =
+        HIR::Toolchain::Archiver::detect_archiver(HIR::Machines::Machine::BUILD, {"ar"});
+    ASSERT_NE(comp, nullptr);
+    ASSERT_EQ(comp->id(), "gnu");
+}
