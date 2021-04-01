826b67783af98b14be6e8fdf73b3273b07dcbf87
Dylan Baker <dylan@pnwbakers.com>
2021-04-01 13:43:51 -0700

    toolchains: simplify

--- src/hir/toolchains/toolchain.cpp
+++ src/hir/toolchains/toolchain.cpp
@@ -1,8 +1,6 @@
 // SPDX-license-identifier: Apache-2.0
 // Copyright © 2021 Intel Corporation
 
-#include <cassert>
-
 #include "archiver.hpp"
 #include "compiler.hpp"
 #include "linker.hpp"
@@ -12,14 +10,10 @@ namespace HIR::Toolchain {
 
 Toolchain get_toolchain(const Language & lang, const Machines::Machine & for_machine) {
     // TODO: handle passing in explicit binary name
-    switch (lang) {
-        case Language::CPP:
-            auto compiler = Compiler::detect_compiler(lang, for_machine);
-            auto archiver = Archiver::detect_archiver(for_machine);
-            auto linker = Linker::detect_linker(compiler, for_machine);
-            return Toolchain{std::move(compiler), std::move(linker), std::move(archiver)};
-    }
-    assert(false);
+    auto compiler = Compiler::detect_compiler(lang, for_machine);
+    auto archiver = Archiver::detect_archiver(for_machine);
+    auto linker = Linker::detect_linker(compiler, for_machine);
+    return Toolchain{std::move(compiler), std::move(linker), std::move(archiver)};
 };
 
 } // namespace HIR::Toolchain
