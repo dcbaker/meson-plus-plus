dac4c05cd16dac5d5571546995e830065c44bc7d
Dylan Baker <dylan@pnwbakers.com>
2021-06-24 21:39:46 -0700

    hir: start adding lowering passes

---
+++ src/hir/passes.cpp
@@ -0,0 +1,14 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Dylan Baker
+
+#include "passes.hpp"
+
+namespace HIR::Passes {
+
+bool branch_pruning(IRList * ir) {
+    bool progress = false;
+
+    return progress;
+};
+
+}
---
+++ src/hir/passes.hpp
@@ -0,0 +1,23 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Dylan Baker
+
+/**
+ * Lowering passes for HIR
+ */
+
+#pragma once
+
+#include "hir.hpp"
+
+namespace HIR::Passes {
+
+/**
+ * Prune dead condition branches
+ *
+ * Once we've been able to lower away conditions in the condition ndoes we want
+ * to trim away dead branches and join the ir lists together so we end up with a
+ * single flat list of Objects.
+ */
+bool branch_pruning(IRList *);
+
+}
