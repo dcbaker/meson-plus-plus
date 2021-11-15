325c4f460e0eb344f14d795a365c946daa09f895
Dylan Baker <dylan@pnwbakers.com>
2021-11-15 14:04:12 -0800

    lower: do cross block optimization
    This makes use of the block walker to do cross block optimizations
    
    Cross block optimization is more complicated than the single block
    optimization we were doing before, but it opens up better performance
    options, as well as reducing the number of passes that we need to make
    over the IR to fully lower it.
    
    Fixes #47
    Fixes #48

--- src/mir/lower.cpp
+++ src/mir/lower.cpp
@@ -2,21 +2,25 @@
 // Copyright © 2021 Intel Corporation
 
 #include "lower.hpp"
+#include "passes/private.hpp"
 
 namespace MIR {
 
 void lower(BasicBlock * block, State::Persistant & pstate) {
-    bool progress;
+    bool progress = false;
     // clang-format off
     do {
-        progress = false
-            || Passes::machine_lower(block, pstate.machines)
-            || Passes::insert_compilers(block, pstate.toolchains)
-            || Passes::flatten(block, pstate)
-            || Passes::lower_free_functions(block, pstate)
-            || Passes::branch_pruning(block)
-            || Passes::join_blocks(block)
-            ;
+        progress = Passes::block_walker(
+            block,
+            {
+                [&](BasicBlock * b) { return Passes::machine_lower(b, pstate.machines); },
+                [&](BasicBlock * b) { return Passes::insert_compilers(b, pstate.toolchains); },
+                [&](BasicBlock * b) { return Passes::flatten(b, pstate); },
+                [&](BasicBlock * b) { return Passes::lower_free_functions(b, pstate); },
+                Passes::branch_pruning,
+                Passes::join_blocks,
+            }
+        );
     } while (progress);
     // clang-format on
 }
