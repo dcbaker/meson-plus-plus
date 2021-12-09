f9f072cbf4a0453d3bdbc42f8c7f911d49c4807d
Dylan Baker <dylan@pnwbakers.com>
2021-12-09 15:45:53 -0800

    mir: fixup Phis after branch pruning
    When we prune a branch, we need to check phis, and make sure that any
    phis pointing to a value that is about to be removed is replaced by an
    identifier to the value not about to be replaced. Then we can use
    constant folding later to do away with any cases of duplicate values.

--- src/mir/passes_test.cpp
+++ src/mir/passes_test.cpp
@@ -209,6 +209,132 @@ TEST(branch_pruning, if_false) {
     ASSERT_EQ(last->var.name, "y");
 }
 
+TEST(branch_pruning, fixup_phi_simple) {
+    auto irlist = lower(R"EOF(
+        if true
+            x = 9
+        else
+            x = 10
+        endif
+        )EOF");
+    std::unordered_map<std::string, uint32_t> data{};
+
+    // We do this in two walks because we don't have all of passes necissary to
+    // get the state we want to test.
+    MIR::Passes::block_walker(
+        &irlist, {
+                     [&](MIR::BasicBlock * b) { return MIR::Passes::value_numbering(b, data); },
+                     [&](MIR::BasicBlock * b) { return MIR::Passes::insert_phis(b, data); },
+                 });
+    MIR::Passes::block_walker(&irlist, {
+                                           MIR::Passes::branch_pruning,
+                                           MIR::Passes::join_blocks,
+                                       });
+
+    ASSERT_EQ(irlist.instructions.size(), 2);
+
+    {
+        const auto & id_obj = irlist.instructions.front();
+        ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Number>>(id_obj));
+        const auto & id = std::get<std::unique_ptr<MIR::Number>>(id_obj);
+        ASSERT_EQ(id->value, 9);
+        ASSERT_EQ(id->var.name, "x");
+        ASSERT_EQ(id->var.version, 1);
+    }
+
+    {
+        const auto & id_obj = irlist.instructions.back();
+        ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Identifier>>(id_obj));
+        const auto & id = std::get<std::unique_ptr<MIR::Identifier>>(id_obj);
+        ASSERT_EQ(id->version, 1);
+        ASSERT_EQ(id->var.name, "x");
+        ASSERT_EQ(id->var.version, 3);
+    }
+}
+
+TEST(branch_pruning, fixup_phi_three_way) {
+    auto irlist = lower(R"EOF(
+        if true
+            x = 9
+        elif y
+            x = 11
+        else
+            x = 10
+        endif
+        )EOF");
+    std::unordered_map<std::string, uint32_t> data{};
+
+    // We do this in two walks because we don't have all of passes necissary to
+    // get the state we want to test.
+    MIR::Passes::block_walker(
+        &irlist, {
+                     [&](MIR::BasicBlock * b) { return MIR::Passes::value_numbering(b, data); },
+                     [&](MIR::BasicBlock * b) { return MIR::Passes::insert_phis(b, data); },
+                 });
+    MIR::Passes::block_walker(&irlist, {
+                                           MIR::Passes::branch_pruning,
+                                           MIR::Passes::join_blocks,
+                                       });
+
+    ASSERT_EQ(irlist.instructions.size(), 3);
+
+    auto it = irlist.instructions.begin();
+
+    {
+        const auto & id_obj = *it;
+        ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Number>>(id_obj));
+        const auto & id = std::get<std::unique_ptr<MIR::Number>>(id_obj);
+        ASSERT_EQ(id->value, 9);
+        ASSERT_EQ(id->var.name, "x");
+        ASSERT_EQ(id->var.version, 1);
+    }
+
+    {
+        const auto & id_obj = *(++it);
+        ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Identifier>>(id_obj));
+        const auto & id = std::get<std::unique_ptr<MIR::Identifier>>(id_obj);
+        ASSERT_EQ(id->version, 1);
+        ASSERT_EQ(id->var.name, "x");
+        ASSERT_EQ(id->var.version, 4);
+    }
+
+    {
+        const auto & id_obj = *(++it);
+        ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Identifier>>(id_obj));
+        const auto & id = std::get<std::unique_ptr<MIR::Identifier>>(id_obj);
+        ASSERT_EQ(id->version, 4);
+        ASSERT_EQ(id->var.name, "x");
+        ASSERT_EQ(id->var.version, 5);
+    }
+}
+
+TEST(branch_purning, fixup_phi_nested_branches) {
+    auto irlist = lower(R"EOF(
+        x = 9
+        if true
+            if true
+                x = 11
+            else
+                x = 10
+            endif
+        endif
+        )EOF");
+    std::unordered_map<std::string, uint32_t> data{};
+
+    bool progress = true;
+    while (progress) {
+        progress = MIR::Passes::block_walker(
+            &irlist, {
+                         [&](MIR::BasicBlock * b) { return MIR::Passes::value_numbering(b, data); },
+                         [&](MIR::BasicBlock * b) { return MIR::Passes::insert_phis(b, data); },
+                         MIR::Passes::branch_pruning,
+                         MIR::Passes::join_blocks,
+                     });
+    }
+
+    ASSERT_EQ(irlist.instructions.size(), 4);
+}
+
 TEST(join_blocks, simple) {
     auto irlist = lower("x = 7\nif true\n x = 8\nelse\n x = 9\nendif\ny = x");
     bool progress = MIR::Passes::block_walker(&irlist, {MIR::Passes::branch_pruning});
