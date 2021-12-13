a32182387dcb99cbe4f8bf58a950b3bebb7f4c16
Dylan Baker <dylan@pnwbakers.com>
2021-12-12 20:24:06 -0800

    mir/passes_test: add another value_numbering test

--- src/mir/passes_test.cpp
+++ src/mir/passes_test.cpp
@@ -670,6 +670,35 @@ TEST(value_numbering, branching) {
     ASSERT_EQ(bb2_val->var.version, 4);
 }
 
+TEST(value_numbering, three_branch) {
+    auto irlist = lower(R"EOF(
+        if true
+            x = 9
+        elif y
+            x = 10
+        else
+            x = 11
+        endif
+        )EOF");
+    std::unordered_map<std::string, uint32_t> data{};
+    MIR::Passes::block_walker(
+        &irlist, {[&](MIR::BasicBlock * b) { return MIR::Passes::value_numbering(b, data); }});
+
+    const auto & bb1 = get_con(irlist.next)->if_true;
+    const auto & bb1_val = std::get<std::unique_ptr<MIR::Number>>(bb1->instructions.front());
+    ASSERT_EQ(bb1_val->var.version, 1);
+
+    const auto & con2 = get_con(get_con(irlist.next)->if_false->next);
+
+    const auto & bb3 = con2->if_true;
+    const auto & bb3_val = std::get<std::unique_ptr<MIR::Number>>(bb3->instructions.front());
+    ASSERT_EQ(bb3_val->var.version, 2);
+
+    const auto & bb2 = con2->if_false;
+    const auto & bb2_val = std::get<std::unique_ptr<MIR::Number>>(bb2->instructions.front());
+    ASSERT_EQ(bb2_val->var.version, 3);
+}
+
 TEST(insert_phi, simple) {
     auto irlist = lower(R"EOF(
         if true
