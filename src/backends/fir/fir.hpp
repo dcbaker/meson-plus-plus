35011555d4d3542bc6631a3342a6227e84d87022
Dylan Baker <dylan@pnwbakers.com>
2022-01-27 14:50:56 -0800

    backends: split the rule into a separate IR module
    The Rule type is really a lower level IR for file-like backends, so I
    created a new fir (File IR) for this purpose. This should be sharable
    with a make-based backend. I'm doing this now to keep from creating
    layering issues later.
    
    Fixes #79

---
+++ src/backends/fir/fir.hpp
@@ -0,0 +1,76 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Intel Corporation
+
+#pragma once
+
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "meson/machines.hpp"
+#include "meson/state/state.hpp"
+#include "mir.hpp"
+
+namespace Backends::FIR {
+
+enum class TargetType {
+    COMPILE,
+    ARCHIVE,
+    LINK,
+    CUSTOM,
+};
+
+/**
+ * A Ninja rule to be generated later
+ */
+class Target {
+  public:
+    Target(const std::vector<std::string> & in, const std::vector<std::string> & out,
+           const TargetType & r, const MIR::Toolchain::Language & l,
+           const MIR::Machines::Machine & m)
+        : input{in}, output{out}, type{r}, lang{l}, machine{m}, arguments{}, deps{},
+          order_deps{} {};
+    Target(const std::vector<std::string> & in, const std::string & out, const TargetType & r,
+           const MIR::Toolchain::Language & l, const MIR::Machines::Machine & m,
+           const std::vector<std::string> & args)
+        : input{in}, output{out}, type{r}, lang{l}, machine{m}, arguments{args}, deps{},
+          order_deps{} {};
+    Target(const std::vector<std::string> & in, const std::string & out, const TargetType & r,
+           const MIR::Toolchain::Language & l, const MIR::Machines::Machine & m,
+           const std::vector<std::string> & args, const std::vector<std::string> & d,
+           const std::vector<std::string> & o)
+        : input{in}, output{out}, type{r}, lang{l}, machine{m}, arguments{args}, deps{d},
+          order_deps{o} {};
+    Target(const std::vector<std::string> & in, const std::vector<std::string> & out,
+           const TargetType & r, const std::vector<std::string> & a)
+        : input{in}, output{out}, type{r}, lang{}, machine{}, arguments{a}, deps{}, order_deps{} {};
+
+    /// The input for this rule
+    const std::vector<std::string> input;
+
+    /// The output of this rule
+    const std::vector<std::string> output;
+
+    /// The type of rule this is
+    const TargetType type;
+
+    /// The language of this rule
+    const MIR::Toolchain::Language lang;
+
+    /// The machine of this rule
+    const MIR::Machines::Machine machine;
+
+    /// The arguments for this rule
+    const std::vector<std::string> arguments;
+
+    /// Order only inputs
+    const std::vector<std::string> deps;
+
+    /// Order only inputs
+    const std::vector<std::string> order_deps;
+};
+
+std::vector<Target> mir_to_fir(const MIR::BasicBlock * const block,
+                               const MIR::State::Persistant & pstate);
+
+} // namespace Backends::FIR
---
+++ src/backends/fir/mir_to_fir.cpp
@@ -0,0 +1,185 @@
+// SPDX-license-identifier: Apache-2.0
+// Copyright © 2021 Intel Corporation
+
+#include "fir.hpp"
+
+namespace Backends::FIR {
+
+namespace {
+
+template <typename T>
+std::vector<Target> target_rule(const T & e, const MIR::State::Persistant & pstate) {
+    static_assert(std::is_base_of<MIR::Executable, T>::value ||
+                      std::is_base_of<MIR::StaticLibrary, T>::value,
+                  "Must be derived from a build target");
+
+    std::vector<std::string> cpp_args{};
+    if (e.arguments.find(MIR::Toolchain::Language::CPP) != e.arguments.end()) {
+        const auto & tc = pstate.toolchains.at(MIR::Toolchain::Language::CPP);
+        for (const auto & a : e.arguments.at(MIR::Toolchain::Language::CPP)) {
+            const auto & args =
+                tc.build()->compiler->specialize_argument(a, pstate.source_root, pstate.build_root);
+            for (const auto & arg : args) {
+                cpp_args.emplace_back(arg);
+            }
+        }
+    }
+
+    std::vector<Target> rules{};
+    const auto & tc = pstate.toolchains.at(MIR::Toolchain::Language::CPP);
+
+    // These are the same on each iteration
+    const auto & always_args = tc.build()->compiler->always_args();
+
+    // TODO: there's a keyword argument to control this
+    auto lincs = tc.build()->compiler->specialize_argument(
+        MIR::Arguments::Argument(e.subdir, MIR::Arguments::Type::INCLUDE), pstate.source_root,
+        pstate.build_root);
+    for (const auto & arg : lincs) {
+        cpp_args.emplace_back(arg);
+    }
+
+    std::vector<std::string> order_deps{};
+    for (const auto & f : e.sources) {
+        if (std::holds_alternative<std::shared_ptr<MIR::CustomTarget>>(f)) {
+            const auto & t = *std::get<std::shared_ptr<MIR::CustomTarget>>(f);
+            for (const auto & ff : t.outputs) {
+                if (tc.build()->compiler->supports_file(ff.get_name()) ==
+                    MIR::Toolchain::Compiler::CanCompileType::DEPENDS) {
+                    order_deps.emplace_back(ff.relative_to_build_dir());
+                }
+            }
+        }
+    }
+
+    std::vector<std::string> srcs{};
+    for (const auto & f : e.sources) {
+        // TODO: obj files are a per compiler thing, I think
+        // TODO: get the proper language
+        // TODO: actually set args to something
+        // TODO: do something better for private dirs, we really need the subdir for this
+
+        auto lang_args = cpp_args;
+        lang_args.insert(lang_args.end(), always_args.begin(), always_args.end());
+
+        // FIXME: without depfile support, we can't really treat order only deps
+        // correctly, and instead we have to treat them as full deps for correct
+        // behavior. This should be fixed.
+        if (std::holds_alternative<std::shared_ptr<MIR::File>>(f)) {
+            const auto & ff = *std::get<std::shared_ptr<MIR::File>>(f);
+            if (tc.build()->compiler->supports_file(ff.get_name()) ==
+                MIR::Toolchain::Compiler::CanCompileType::SOURCE) {
+                rules.emplace_back(
+                    Target{{ff.relative_to_build_dir()},
+                           std::string{fs::path{e.name + ".p"} / ff.get_name()} + ".o",
+                           TargetType::COMPILE,
+                           MIR::Toolchain::Language::CPP,
+                           MIR::Machines::Machine::BUILD,
+                           lang_args,
+                           {},
+                           order_deps});
+            }
+        } else {
+            const auto & t = *std::get<std::shared_ptr<MIR::CustomTarget>>(f);
+            for (const auto & ff : t.outputs) {
+                if (tc.build()->compiler->supports_file(ff.get_name()) ==
+                    MIR::Toolchain::Compiler::CanCompileType::SOURCE) {
+                    rules.emplace_back(
+                        Target{{ff.relative_to_build_dir()},
+                               std::string{fs::path{e.name + ".p"} / ff.get_name()} + ".o",
+                               TargetType::COMPILE,
+                               MIR::Toolchain::Language::CPP,
+                               MIR::Machines::Machine::BUILD,
+                               lang_args,
+                               {ff.relative_to_build_dir()},
+                               order_deps});
+                }
+            }
+        }
+    }
+
+    std::vector<std::string> final_outs;
+    for (const auto & r : rules) {
+        final_outs.insert(final_outs.end(), r.output.begin(), r.output.end());
+    }
+    for (const auto & [_, l] : e.link_static) {
+        final_outs.emplace_back(l->output());
+    }
+
+    std::string name;
+    TargetType type;
+    std::vector<std::string> link_args{};
+    if constexpr (std::is_base_of<MIR::StaticLibrary, T>::value) {
+        type = TargetType::ARCHIVE;
+        // TODO: per platform?
+        name = e.output();
+        // TODO: need to combin with link_arguments from DSL
+        link_args = tc.build()->archiver->always_args();
+    } else {
+        type = TargetType::LINK;
+        name = e.output();
+        link_args = tc.build()->linker->always_args();
+    }
+
+    // TODO: linker/archiver always_args
+
+    rules.emplace_back(Target{
+        final_outs,
+        name,
+        type,
+        MIR::Toolchain::Language::CPP,
+        MIR::Machines::Machine::BUILD,
+        link_args,
+    });
+
+    return rules;
+}
+
+template <>
+std::vector<Target> target_rule<MIR::CustomTarget>(const MIR::CustomTarget & e,
+                                                   const MIR::State::Persistant & pstate) {
+    std::vector<std::string> outs{};
+    for (const auto & o : e.outputs) {
+        outs.emplace_back(o.relative_to_build_dir());
+    }
+
+    std::vector<std::string> ins{};
+    for (const auto & i : e.inputs) {
+        if (std::holds_alternative<std::shared_ptr<MIR::File>>(i)) {
+            const auto & f = *std::get<std::shared_ptr<MIR::File>>(i);
+            ins.emplace_back(f.relative_to_build_dir());
+        } else {
+            const auto & c = *std::get<std::shared_ptr<MIR::CustomTarget>>(i);
+            for (const auto & f : c.outputs) {
+                ins.emplace_back(f.relative_to_build_dir());
+            }
+        }
+    }
+
+    return {Target{ins, outs, TargetType::CUSTOM, e.command}};
+}
+
+} // namespace
+
+std::vector<Target> mir_to_fir(const MIR::BasicBlock * const block,
+                               const MIR::State::Persistant & pstate) {
+    // A list of all rules
+    std::vector<Target> rules{};
+
+    for (const auto & i : block->instructions) {
+        if (std::holds_alternative<std::shared_ptr<MIR::Executable>>(i)) {
+            auto r = target_rule(*std::get<std::shared_ptr<MIR::Executable>>(i), pstate);
+            std::move(r.begin(), r.end(), std::back_inserter(rules));
+        } else if (std::holds_alternative<std::shared_ptr<MIR::StaticLibrary>>(i)) {
+            auto r = target_rule(*std::get<std::shared_ptr<MIR::StaticLibrary>>(i), pstate);
+            std::move(r.begin(), r.end(), std::back_inserter(rules));
+        } else if (std::holds_alternative<std::shared_ptr<MIR::CustomTarget>>(i)) {
+            auto r = target_rule(*std::get<std::shared_ptr<MIR::CustomTarget>>(i), pstate);
+            std::move(r.begin(), r.end(), std::back_inserter(rules));
+        }
+    }
+
+    return rules;
+}
+
+} // namespace Backends::FIR
