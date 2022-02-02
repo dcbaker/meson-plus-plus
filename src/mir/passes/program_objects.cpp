621b9864d0c5677348b8c520cdeb3e87a0944ec3
Dylan Baker <dylan@pnwbakers.com>
2022-02-01 16:26:18 -0800

    mir/passes/program_objects: Also use a single dispatch function
    To further slightly reduce the overhead.

--- src/mir/passes/program_objects.cpp
+++ src/mir/passes/program_objects.cpp
@@ -9,42 +9,47 @@ namespace MIR::Passes {
 
 namespace {
 
-std::optional<Object> lower_found_method(const Object & obj) {
-    if (!std::holds_alternative<std::shared_ptr<FunctionCall>>(obj)) {
-        return std::nullopt;
-    }
-    const auto & f = std::get<std::shared_ptr<FunctionCall>>(obj);
-
-    if (!(f->holder.has_value() &&
-          std::holds_alternative<std::shared_ptr<Program>>(f->holder.value())) ||
-        f->name != "found") {
-        return std::nullopt;
-    }
-
-    if (!f->pos_args.empty()) {
+std::optional<Object> lower_found_method(const FunctionCall & f) {
+    if (!f.pos_args.empty()) {
         throw Util::Exceptions::InvalidArguments(
             "Program.found() does not take any positional arguments");
     }
-    if (!f->kw_args.empty()) {
+    if (!f.kw_args.empty()) {
         throw Util::Exceptions::InvalidArguments(
             "Program.found() does not take any keyword arguments");
     }
 
-    return std::make_shared<Boolean>(
-        std::get<std::shared_ptr<Program>>(f->holder.value())->found());
+    return std::make_shared<Boolean>(std::get<std::shared_ptr<Program>>(f.holder.value())->found());
 }
 
+std::optional<Object> lower_program_methods_impl(const Object & obj,
+                                                 const State::Persistant & pstate) {
+    if (!std::holds_alternative<std::shared_ptr<FunctionCall>>(obj)) {
+        return std::nullopt;
+    }
+    const auto & f = *std::get<std::shared_ptr<FunctionCall>>(obj);
+
+    if (!(f.holder.has_value() &&
+          std::holds_alternative<std::shared_ptr<Program>>(f.holder.value()))) {
+        return std::nullopt;
+    }
+
+    if (!all_args_reduced(f.pos_args, f.kw_args)) {
+        return std::nullopt;
+    }
+
+    if (f.name == "found") {
+        return lower_found_method(f);
+    }
+
+    // XXX: Shouldn't really be able to get here...
+    return std::nullopt;
+}
 } // namespace
 
 bool lower_program_objects(BasicBlock & block, State::Persistant & pstate) {
-    bool progress = false;
-    // clang-format off
-    return false
-        || function_walker(&block, lower_found_method)
-        ;
-    // clang-format on
-
-    return progress;
+    return function_walker(
+        &block, [&](const Object & obj) { return lower_program_methods_impl(obj, pstate); });
 }
 
 } // namespace MIR::Passes
