ee3f53c7124eb42de85db0aeb3db6d4b24d76ff4
Dylan Baker <dylan@pnwbakers.com>
2022-02-02 11:35:15 -0800

    mir/passes: add an unreachable code pass
    This clears dead code created by anything following an error. We know
    that once the error is guaranteed to be executed that anything after
    that should not be. This means that we can delete any instructions after
    the error, and break any next pointer the block may have.

---
+++ src/mir/passes/tests/dead_code_test.cpp
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2022 Dylan Baker
+
+#include <gtest/gtest.h>
+
+#include "passes.hpp"
+#include "passes/private.hpp"
+#include "state/state.hpp"
+
+#include "test_utils.hpp"
+
+TEST(unreachable_code, clear_dead_instructions) {
+    auto irlist = lower(R"EOF(
+        message('bar')
+        error('should be dead')
+        warning('should be deleted')
+        )EOF");
+
+    MIR::State::Persistant pstate{"", ""};
+
+    MIR::Passes::block_walker(
+        &irlist,
+        {
+            [&](MIR::BasicBlock * b) { return MIR::Passes::lower_free_functions(b, pstate); },
+            [](MIR::BasicBlock * b) { return MIR::Passes::delete_unreachable(*b); },
+        });
+
+    ASSERT_EQ(irlist.instructions.size(), 2);
+
+    const auto & msg_obj = irlist.instructions.front();
+    ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Message>>(msg_obj));
+    const auto & msg = *std::get<std::unique_ptr<MIR::Message>>(msg_obj);
+    ASSERT_EQ(msg.level, MIR::MessageLevel::MESSAGE);
+
+    const auto & err_obj = irlist.instructions.back();
+    ASSERT_TRUE(std::holds_alternative<std::unique_ptr<MIR::Message>>(err_obj));
+    const auto & err = *std::get<std::unique_ptr<MIR::Message>>(err_obj);
+    ASSERT_EQ(err.level, MIR::MessageLevel::ERROR);
+}
+
+TEST(unreachable_code, clear_next) {
+    auto irlist = lower(R"EOF(
+        if some_var
+            error('foo')
+        else
+            x = 10
+        endif
+        message(x)
+        )EOF");
+
+    MIR::State::Persistant pstate{"", ""};
+
+    MIR::Passes::block_walker(
+        &irlist,
+        {
+            [&](MIR::BasicBlock * b) { return MIR::Passes::lower_free_functions(b, pstate); },
+            [](MIR::BasicBlock * b) { return MIR::Passes::delete_unreachable(*b); },
+        });
+
+    const auto & branch = *get_bb(get_con(irlist.next)->if_true);
+    ASSERT_EQ(branch.instructions.size(), 1);
+    ASSERT_TRUE(std::holds_alternative<std::monostate>(branch.next));
+}
