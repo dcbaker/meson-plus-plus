7c52fcd5a54bb76a0aba26cb991aa89015d1c2e5
Dylan Baker <dylan@pnwbakers.com>
2022-12-02 15:34:53 -0800

    mir/passes/dead_code: don't run the unreachable infinitely
    Right now dead code will be triggered to run over and over again when it
    spots an error message, like from lowering away assert. That's obviously
    bad.

--- src/mir/passes/dead_code.cpp
+++ src/mir/passes/dead_code.cpp
@@ -13,24 +13,36 @@ bool delete_unreachable(BasicBlock & block) {
     for (auto itr = block.instructions.begin(); itr != block.instructions.end(); ++itr) {
         if (auto * m = std::get_if<Message>(itr->obj_ptr.get())) {
             if (m->level == MessageLevel::ERROR) {
+                bool progress = false;
+
                 // Delete any children point to this block
                 if (std::holds_alternative<std::shared_ptr<BasicBlock>>(block.next)) {
                     auto & b = *std::get<std::shared_ptr<BasicBlock>>(block.next);
                     b.parents.erase(&block);
+                    progress = true;
                 } else if (std::holds_alternative<std::unique_ptr<Condition>>(block.next)) {
                     const auto & con = *std::get<std::unique_ptr<Condition>>(block.next);
                     for (const auto & c : {con.if_true, con.if_false}) {
                         c->parents.erase(&block);
                     }
+                    progress = true;
                 }
-                // Set next to monostate, there is nothing after this
-                block.next = std::monostate{};
 
-                // Delete all instructions after this message, they don't actually exists
-                // This may delete additional errors, but we can't be sure
-                // they're not spurious or caused by the first error
-                block.instructions.erase(++itr, block.instructions.end());
-                return true;
+                // Set next to monostate, there is nothing after this
+                if (!std::holds_alternative<std::monostate>(block.next)) {
+                    progress = true;
+                    block.next = std::monostate{};
+                }
+
+                if (++itr != block.instructions.end()) {
+                    // Delete all instructions after this message, they don't actually exists
+                    // This may delete additional errors, but we can't be sure
+                    // they're not spurious or caused by the first error
+                    block.instructions.erase(itr, block.instructions.end());
+                    progress = true;
+                }
+
+                return progress;
             }
         }
     }
