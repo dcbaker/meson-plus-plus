ee218dd76a264abbd91f5d1d6d5c9af43b2c8d28
Dylan Baker <dylan@pnwbakers.com>
2024-04-26 13:58:29 -0700

    Fix SPDX-License-Identifier spelling

--- src/mir/meson/state/state.cpp
+++ src/mir/meson/state/state.cpp
@@ -1,5 +1,5 @@
-// SPDX-license-identifier: Apache-2.0
-// Copyright © 2021-2022 Dylan Baker
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2021-2024 Intel Corporation
 
 #include "state.hpp"
 
--- src/mir/passes/value_numbering.cpp
+++ src/mir/passes/value_numbering.cpp
@@ -1,5 +1,5 @@
-// SPDX-license-identifier: Apache-2.0
-// Copyright © 2021 Dylan Baker
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2021-2024 Intel Corporation
 
 #include "passes.hpp"
 #include "private.hpp"
