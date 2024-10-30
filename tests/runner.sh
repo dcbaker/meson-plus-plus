68d87ca7d56288c9f50ba6e2845b83460817e353
Dylan Baker <dylan@pnwbakers.com>
2024-10-30 10:44:58 -0700

    tests: Add support for simple setup and teardown scripts
    This is not the long term solution I want, but it's a start

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-MESONPP=${1}
-TESTDIR=${2}
+MESONPP="${1}"
+TESTDIR="${2}"
 BUILDDIR=`mktemp -d`
+SETUP_SH="${TESTDIR}/setup.sh"
+TEARDOWN_SH="${TESTDIR}/teardown.sh"
 
 function cleanup() {
     rm -rf "${BUILDDIR}"
+    if [ -f "${TEARDOWN_SH}" ]; then
+        bash "${TEARDOWN_SH}" "${TESTDIR}"
+    fi
 }
-trap cleanup exit
+trap 'cleanup' exit
+
+if [ -f "${SETUP_SH}" ]; then
+    bash "${SETUP_SH}" "${TESTDIR}"
+fi
 
 ${MESONPP} configure "${BUILDDIR}" -s "${TESTDIR}"
 ninja -C "${BUILDDIR}"
