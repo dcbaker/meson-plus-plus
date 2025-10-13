bb71a4577b8fe3ca061e44601ecad6b0f64d8aac
Dylan Baker <dylan@pnwbakers.com>
2025-10-13 10:20:08 -0700

    mir: Add stubs for an AST -> MIR conversion pass

---
+++ src/mir/ast_to_mir.cpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2025 Intel Corporation
+
+#include "ast_to_mir.hpp"
+
+namespace MIR {
+
+namespace {
+
+struct ExpressionLowering {};
+
+} // namespace
+
+IR::CFG ast_to_mir(const std::unique_ptr<Frontend::AST::CodeBlock> & block) {
+    return IR::CFG{std::make_shared<IR::Node>(std::make_shared<IR::BasicBlock>())};
+}
+
+} // namespace MIR
---
+++ src/mir/ast_to_mir.hpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2025 Intel Corporation
+
+#pragma once
+
+#include "ir/ir.hpp"
+#include "node.hpp"
+
+namespace MIR {
+
+IR::CFG ast_to_mir(const std::unique_ptr<Frontend::AST::CodeBlock> & block);
+
+}
