d40cf1ea5096b29b9a6171badb24f0935464f379
Dylan Baker <dylan@pnwbakers.com>
2025-10-14 14:44:40 -0700

    mir/ir/operation: Start implementing some operation pieces

--- src/mir/ir/operation.cpp
+++ src/mir/ir/operation.cpp
@@ -4,11 +4,34 @@
 #include "operation.hpp"
 
 #include <sstream>
+#include <stdexcept>
 
 namespace MIR::IR {
 
-Operation::Operation() = default;
+namespace {
 
-std::string Operation::serialize() const { return "Operation { }"; }
+std::string to_string(OperationType t) {
+    switch (t) {
+        case OperationType::get_attribute:
+            return "getattr";
+        default:
+            throw std::runtime_error("Unknown operation type");
+    }
+}
+
+} // namespace
+
+Operation::Operation(Instruction l, OperationType t, Instruction r)
+    : left{std::move(l)}, type{t}, right{std::move(r)} {};
+
+std::string Operation::serialize() const {
+    std::stringstream ss{};
+    ss << "Operation { "
+       << "left = { " << left.serialize() << " }"
+       << "type = { " << to_string(type) << " }"
+       << "right = { " << right.serialize() << " }"
+       << " }";
+    return ss.str();
+}
 
 } // namespace MIR::IR
--- src/mir/ir/operation.hpp
+++ src/mir/ir/operation.hpp
@@ -5,17 +5,30 @@
 
 #include <string>
 
+#include "instruction.hpp"
+
 namespace MIR::IR {
+
+enum class OperationType {
+    // Maps to <object>.<attribute>
+    get_attribute,
+};
 
 /// @brief An operation that does not create a target
 ///
 /// These are pure, they don't affect
+
+// TODO: should this be InstructionType or Instruction?
 class Operation {
   public:
-    Operation();
+    Operation(Instruction left, OperationType t, Instruction right);
 
     /// @brief provide a serialized form of this instruction
     std::string serialize() const;
+
+    Instruction left;
+    OperationType type;
+    Instruction right;
 };
 
 } // namespace MIR::IR
