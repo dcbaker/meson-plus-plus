26504862690a177e95260b5e0e26bd4718d49cb8
Dylan Baker <dylan@pnwbakers.com>
2026-04-08 10:43:57 -0700

    mir: Add stubs for all AST -> MIR lowering functions

--- src/mir/ast_to_mir.cpp
+++ src/mir/ast_to_mir.cpp
@@ -7,11 +7,42 @@ namespace MIR {
 
 namespace {
 
+using namespace Frontend;
+
 /// @brief Lower AST expressions into MIR representations
-struct ExpressionLowering {};
+struct ExpressionLowering {
+    IR::InstructionType operator()(const std::unique_ptr<AST::AdditiveExpression> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Boolean> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Identifier> & stmt) const;
+    IR::InstructionType
+    operator()(const std::unique_ptr<AST::MultiplicativeExpression> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::UnaryExpression> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Number> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::String> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Subscript> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Relational> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::FunctionCall> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::GetAttribute> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Array> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Dict> & stmt) const;
+    IR::InstructionType operator()(const std::unique_ptr<AST::Ternary> & stmt) const;
+};
 
 /// @brief Lower AST statements into MIR representations
-struct StatementLowering {};
+struct StatementLowering {
+
+    StatementLowering() : el{} {};
+
+    IR::Instruction operator()(const std::unique_ptr<AST::Statement> & stmt) const;
+    IR::Instruction operator()(const std::unique_ptr<AST::Assignment> & stmt) const;
+    IR::Instruction operator()(const std::unique_ptr<AST::IfStatement> & stmt) const;
+    IR::Instruction operator()(const std::unique_ptr<AST::ForeachStatement> & stmt) const;
+    IR::Instruction operator()(const std::unique_ptr<AST::Break> & stmt) const;
+    IR::Instruction operator()(const std::unique_ptr<AST::Continue> & stmt) const;
+
+  private:
+    const ExpressionLowering el;
+};
 
 } // namespace
 
