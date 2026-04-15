d256e980fd8afbdafe385106c99627928616bd07
Dylan Baker <dylan@pnwbakers.com>
2026-04-15 12:00:52 -0700

    mir/builder: Add some tests for the various builder constructors

--- src/mir/builder/builder_test.cpp
+++ src/mir/builder/builder_test.cpp
@@ -3,11 +3,13 @@
 
 #include "builder.hpp"
 #include "ir.hpp"
+#include "test_helpers.hpp"
 
 #include <gtest/gtest.h>
 
 using namespace MIR::IR;
 using namespace MIR::builder;
+using namespace MIR::UT;
 
 TEST(MIR_Builder, simple) {
     Builder b{};
@@ -16,6 +18,55 @@ TEST(MIR_Builder, simple) {
     auto n = b.get();
     auto && insts = n->block->instructions;
     ASSERT_EQ(insts.size(), 1);
+}
+
+TEST(MIR_Builder, constructor_from_non_empty_block_no_condition) {
+    Builder b{};
+
+    // clang-format off
+    b.add_inst(make_instruction<Number>(0))
+     .set_cursor_begin()
+     .add_inst(make_instruction<Number>(1));
+    // clang-format on
+
+    b = {b.get()};
+    auto itr = b.get()->block->instructions.begin();
+    EXPECT_EQ(std::get<std::shared_ptr<Number>>((*itr++)->instruction)->value, 1);
+    EXPECT_EQ(std::get<std::shared_ptr<Number>>((*itr++)->instruction)->value, 0);
+
+    b.add_inst(make_instruction<Number>(42));
+    EXPECT_EQ(get<Number>(get_ir(b.get()->block, -1).instruction).value, 42);
+}
+
+TEST(MIR_Builder, constructor_from_non_empty_block_with_condition) {
+    Builder b{};
+
+    // clang-format off
+    b.add_inst(make_instruction<Number>(0))
+     .set_cursor_begin()
+     .add_inst(make_instruction<Number>(1))
+     .add_condition(make_instruction<Boolean>(true));
+    // clang-format on
+
+    b = {b.get()};
+    auto itr = b.get()->block->instructions.begin();
+    EXPECT_EQ(b.get()->block->instructions.size(), 3);
+    EXPECT_EQ(std::get<std::shared_ptr<Number>>((*itr++)->instruction)->value, 1);
+    EXPECT_EQ(std::get<std::shared_ptr<Number>>((*itr++)->instruction)->value, 0);
+    EXPECT_EQ(std::get<std::shared_ptr<Boolean>>((*itr++)->instruction)->value, true);
+
+    b.add_inst(make_instruction<Number>(42));
+    EXPECT_EQ(get<Number>(get_ir(b.get()->block, -2).instruction).value, 42);
+    EXPECT_EQ(get<Boolean>(get_ir(b.get()->block, -1).instruction).value, true);
+}
+
+TEST(MIR_Builder, constructor_from_empty_block) {
+    Builder b{std::make_shared<Node>()};
+
+    EXPECT_EQ(b.get()->block->instructions.size(), 0);
+
+    b.add_inst(make_instruction<Number>(42));
+    EXPECT_EQ(get<Number>(get_ir(b.get()->block, -1).instruction).value, 42);
 }
 
 TEST(MIR_Builder, make_instruction) {
